3765435144e59408d0dc8324b08e035d26bc58ee
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-11-07 23:45:33 -0700

    serve statics

--- Startup.cs
+++ Startup.cs
@@ -16,13 +16,21 @@ namespace DutchTreat
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // middleware runs in the order specified here. Order matters.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {   
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Only if you're using html directly, not cshtml
+            app.UseDefaultFiles();  // automatically bind default files
 
+            // ASP.NET core doesn't serve static files by default, it must be configured
+            // This makes sense because API projects which are very common nowdays don't serve statics, or at least they ought not to
+            app.UseStaticFiles();
+
+            /*
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -32,6 +40,8 @@ namespace DutchTreat
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            */
+
         }
     }
 }
