d6566069c252c705a7e7cfdb40225167a3d99877
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-12-07 03:25:58 -0700

    javascript events

---
+++ wwwroot/js/index.js
@@ -0,0 +1,6 @@
+﻿let form = document.getElementById("myForm");
+form.hidden = true;
+
+let buyButton = document.getElementById("buyButton");
+// buyButton.onclick = (e) => alert("clicked");
+buyButton.addEventListener("click", (e) => alert("buying item"));
