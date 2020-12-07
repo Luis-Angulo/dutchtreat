aa2553dc0e1c03ea4774b252ad9ce42f0b980081
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-12-07 03:58:19 -0700

    using npm with .net core

--- Startup.cs
+++ Startup.cs
@@ -24,11 +24,14 @@ namespace DutchTreat
                 app.UseDeveloperExceptionPage();
             }
             // Only if you're using html directly, not cshtml
-            app.UseDefaultFiles();  // automatically bind default files
+            app
+               .UseDefaultFiles()  // automatically bind default files
 
             // ASP.NET core doesn't serve static files by default, it must be configured
             // This makes sense because API projects which are very common nowdays don't serve statics, or at least they ought not to
-            app.UseStaticFiles();
+               .UseStaticFiles()
+               // shortcut from the course, in a real app we should build the npm production dependencies we need, put them in a dist or src folder and serve those as statics
+            .UseNodeModules();
 
             /*
             app.UseRouting();
