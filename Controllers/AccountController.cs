15b809cac481a6446f229d3fa4c4d56dd65fbcdc
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-12-29 11:17:11 -0700

    Adding login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using DutchTreat.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
-        public AccountController(ILogger<AccountController> logger)
+        private readonly SignInManager<StoreUser> _signInManager;
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
+        [HttpGet]
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
@@ -18,5 +26,33 @@ namespace DutchTreat.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager
+                .PasswordSignInAsync(
+                    vmodel.Username,
+                    vmodel.Password,
+                    vmodel.RememberMe,
+                    false);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Failed to login");
+            return View();
+        }
+
     }
 }
